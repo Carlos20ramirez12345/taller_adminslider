d454a080fd68c465a9d3f49d272160a18dd31f0d
CarlosRamirez <negocioshotmart.caliche2020@gmail.com>
2021-09-27 19:05:48 -0500

    segundo commit

---
+++ apps/user/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from import_export import resources
+from  import_export.admin import ImportExportModelAdmin
+from apps.user.models import UserApp
+
+# Register your models here.
+
+class UserResource(resources.ModelResource):
+	class Meta:
+		model = UserApp
+
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+	search_fields = ("name", "last_name", "usuid", "rol")
+	list_display = ("name", "last_name", "usuid", "rol")
+	list_filter = ("usuid", "rol")
+	resource_class = UserResource
+
+admin.site.register(UserApp, UserAdmin)
