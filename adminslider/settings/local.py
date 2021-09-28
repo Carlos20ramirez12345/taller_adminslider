6f65c3d1ad9c09ad4affeb7de575d2542c08b5c4
Carlos20ramirez12345 <79713936+Carlos20ramirez12345@users.noreply.github.com>
2021-09-27 19:25:17 -0500

    Update local.py

--- adminslider/settings/local.py
+++ adminslider/settings/local.py
@@ -10,11 +10,7 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'adminslider',
-        'USER': 'root',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'adminslider',
     }
 }
